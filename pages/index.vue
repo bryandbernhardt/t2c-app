7a68dab634cf1f5173e936856a7f6d8d75587640
Bryan Dietrich Bernhardt <bryan@minhavisita.com.br>
2023-11-22 22:58:41 -0300

    chore(index): ajustar layout com ionic

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,30 @@
 <template>
-  <IonPage class="index-page">
-    <IonContent class="index-content">
-      <img src="/t2c-logo.png"  />
-      <p>Seu melhor analisador de dados</p>
-      <IonButton @click="navigateTo('/dashboard')">Iniciar</IonButton>
-    </IonContent>
-  </IonPage>
+  <ion-page class="index-page">
+    <ion-content class="index-content ion-padding">
+      <ion-grid class="grid">
+        <ion-col class="ion-justify-content-around">
+          <ion-row class="ion-justify-content-center">
+            <ion-img src="/t2c-logo.png" class="img" />
+          </ion-row>
+          <ion-row class="ion-justify-content-center">
+            <ion-text>Seu melhor analisador de dados</ion-text>
+          </ion-row>
+          <ion-row class="ion-justify-content-center">
+            <ion-button
+              shape="round"
+              @click="navigateTo('/dashboard')"
+            >
+              Iniciar
+            </ion-button>
+          </ion-row>
+        </ion-col>
+      </ion-grid>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script setup>
-import { IonPage, IonContent, IonButton } from '@ionic/vue';
+import { IonPage, IonContent, IonButton, IonRow, IonCol, IonImg, IonGrid, IonText } from '@ionic/vue';
 </script>
 
 <style lang="scss" scoped>
@@ -19,32 +34,31 @@ ion-content {
 }
 </style>
 
-<style lang="scss">
-// se tentar estilizar normal por aqui, o css padrão do ionic vai sobrescrever
-.index-page {
+<style lang="scss" scoped>
+ion-button {
+  --background: #33A64C;
+  --background-hover: #33a64cb4;
+}
 
-  .index-content {
+ion-grid {
+  height: 100%;
+}
 
-    img {
-      width: 90%;
-      max-width: 15rem;
-    }
+ion-col {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
 
-    p {
-      color: #fff;
-      background-color: #000;
-      padding: 1rem;
-      border-radius: 0.5rem;
-      text-align: center;
-    }
+ion-img {
+  width: 90%;
+  max-width: 15rem;
+}
 
-    button {
-      color: #fff;
-      border: none;
-      border-radius: 2rem;
-      padding: 1rem 2rem;
-      background-color: #33A64D;
-    }
-  }
+ion-text {
+  color: #fff;
+  background-color: #000;
+  padding: 1rem;
+  border-radius: 0.5rem;
 }
 </style>
